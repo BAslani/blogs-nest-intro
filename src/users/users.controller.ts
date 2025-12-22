47ab6ec3690eaf1c16af3deb7c2a831e7340e427
baslani <behzadaslani80@gmail.com>
2025-12-22 15:15:21 +0330

    [wip] implementing users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,27 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Ip,
+  Param,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get('/:id')
-  public getUsers(@Param() params: any, @Query() query: any) {
-    console.log(query);
+  public getUsers(@Param('id') id: string, @Query('name') name: string) {
+    console.log(id);
+    console.log(name);
 
     return 'response from the get users endpoint';
   }
 
   @Post()
-  public createUser(@Body() body: any) {
-    console.log(body);
+  public createUser(@Body() body: any, @Headers() headers: any, @Ip() ip: any) {
+    console.log(ip);
     return 'response from the post user endpoint';
   }
 }
